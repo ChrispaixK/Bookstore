433079c44567c4df7d0aa9761bb35e263512c4bd
Chrispaix KAZE <kchrispaixs@gmail.com>
2022-08-11 12:05:45 +0200

    Add react router

--- src/App.js
+++ src/App.js
@@ -1,28 +1,47 @@
-import logo from './logo.svg';
+import { Routes, Route, Link } from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit
-          {' '}
-          <code>src/App.js</code>
-          {' '}
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="top-bar">
+        <h1>Bookstore</h1>
+        <nav className="links">
+          <Link to="/">Books</Link>
+          <Link to="/Categories">Categories</Link>
+        </nav>
+      </div>
+
+      <Routes>
+        <Route path="/" element={<Books />} />
+        <Route path="Categories" element={<Categories />} />
+      </Routes>
+      <div className="footer">
+        <p>&copy; Bookstore 2022</p>
+      </div>
     </div>
+  );
+}
+
+function Books() {
+  return (
+    <>
+      <main className="books-container">
+        <h2>Books page here</h2>
+      </main>
+    </>
+  );
+}
+
+function Categories() {
+  return (
+    <>
+      <main>
+        <div className="categories-container">
+          <h2>Categories page here</h2>
+        </div>
+      </main>
+    </>
   );
 }
 
